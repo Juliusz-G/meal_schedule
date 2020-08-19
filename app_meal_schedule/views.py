0f069935046c9a96c255a9d3bb45b15474bf9256
Juliusz-G <julek.gorzen@gmail.com>
2020-08-19 10:00:13 +0200

    Landing page view + CSS

--- app_meal_schedule/views.py
+++ app_meal_schedule/views.py
@@ -1,3 +1,12 @@
+
+# Django
+from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class LandingPage(View):
+
+    def get(self, request):
+        return render(request, "index.html", {})
